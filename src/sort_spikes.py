e6994a4e84cc54d94c84f966a7bc3b45f115b7f5
Predrag Živadinović <predrag.zivadinovic@ist.ac.at>
2022-03-23 09:59:04 +0100

    Convert output to phy.

--- src/sort_spikes.py
+++ src/sort_spikes.py
@@ -11,8 +11,10 @@ from spikeinterface.full import concatenate_recordings
 import spikeinterface.extractors as se
 import spikeinterface.sorters as ss
 from spikeinterface.core import load_extractor
+from spikeinterface.toolkit.preprocessing import bandpass_filter
 from joblib import Parallel, delayed
 import src.implants as implants
+from src.utils import convert_to_phy
 
 
 def __write_params(args):
@@ -43,15 +45,20 @@ def __write_segment_shifts(ss, path):
         f.write(ss_str)
 
 
-def __sort_tetrodes(det_thr, tet_recs, out_path, n_jobs):
+def __sort_tetrodes(det_thr, tet_recs, out_path, n_jobs, bp_min=300, bp_max=6000):
     def __run(tn, tr, dt, op):
         print(f"Sorting tetrode {tn}.")
-        return tn, ss.run_sorter("mountainsort4", load_extractor(tr),\
-                      join(op, f"tet{tn}"),\
+        tet_op = join(op, f"tet{tn}")
+        res = ss.run_sorter("mountainsort4", tr,\
+                      tet_op,\
                       with_output=True, remove_existing_folder=True,\
-                      **{"detect_threshold": dt})
+                      **{"detect_threshold": dt, "freq_min": bp_min, "freq_max": bp_max})
+        tv_tr = tr if tr.get_annotation("is_filtered") else\
+                bandpass_filter(tr, bp_min, bp_max)
+        convert_to_phy(tv_tr, res, tet_op)
+        return tn, res
 
-    return dict(Parallel(n_jobs=n_jobs, backend="threading")(delayed(__run)(tn, tr.to_dict(), det_thr, out_path) for tn, tr in tet_recs.items()))
+    return dict(Parallel(n_jobs=n_jobs, backend="threading")(delayed(__run)(tn, tr, det_thr, out_path) for tn, tr in tet_recs.items()))
 
 
 def __print_results(results):
@@ -66,7 +73,10 @@ if __name__ == "__main__":
     args.add_argument("drive", type=str, help=f"Path to drive layout file or name of one of predefined drives: {implants.DEFINED_IMPLANTS}.")
     args.add_argument("out_path", help="Output path for sorted data.")
     args.add_argument("--det_thr", type=float, default=5, help="Threshold for spike detection (default is 5).")
+    args.add_argument("--bp_min", type=float, default=300, help="Lower threshold for bandpass filter (default is 300).")
+    args.add_argument("--bp_max", type=float, default=6000, help="Upper threshold for bandpass filter (default is 6000).")
     args.add_argument("--n_jobs", type=int, default=1, help=f"Number of parallel sorting jobs to run (defauls is {1}).")
+    #args.add_argument("--format", default="phy", help=f"Format for the output data, one of: 'phy'(default).")
     args = args.parse_args()
     __write_params(args)
 
@@ -78,5 +88,7 @@ if __name__ == "__main__":
     rec_c = concatenate_recordings([rec]).set_probegroup(layout, group_mode="by_probe")
     rec_per_tet = rec_c.split_by(property="group")
 
-    results = __sort_tetrodes(args.det_thr, rec_per_tet, args.out_path, args.n_jobs)
+    results = __sort_tetrodes(args.det_thr, rec_per_tet, args.out_path,\
+                args.n_jobs, args.bp_min, args.bp_max)
     __print_results(results)
+    assert rec_per_tet.keys() == results.keys()
