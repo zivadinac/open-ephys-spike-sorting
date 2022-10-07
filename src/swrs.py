116e3a2070c7b6de41a69fe85a9ae69c556d1e4e
Predrag Živadinović <predrag.zivadinovic@ist.ac.at>
2022-10-07 16:21:56 +0200

    Add support for SWRs.

---
+++ src/swrs.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+import numpy as np
+from jcl.load import readfromtxt
+
+
+def find_SWRs(basedir):
+    """ Traverses 'basedir' saved by OpenEphys GUI to find '.sw' files.
+
+        Args:
+            basedir - dir with recorded data
+
+        Return:
+            dictionary {recording: list of SWRs}
+    """
+    p = Path(basedir)
+    recordings = p.glob("*/experiment1/recording*")
+    return {r.name: readfromtxt(list(r.glob("continuous/*/continuous.sw"))[0],
+                                lambda l: [int(n) for n in l.strip().split()])
+            for r in recordings}
+
+
+def merge_SWRs(swrs, resofs):
+    """ Merges given SWRs while shifting timestamps based on 'resofs'.
+
+        Args:
+            swrs - dictionary of SWRs {recording: list of SWRs}
+            resofs - list of recording (session) ending timestamps
+
+        Return:
+            Merged SWRs, np.ndarray of shape (N, 3)
+    """
+    assert len(swrs) == len(resofs)
+    swrs = {int(r.strip("recording")): s for r,s in swrs.items()}
+    resofs = [0] + resofs
+    swrs_list = []
+    for i in swrs.keys():
+        swrs_i = np.array(swrs[i])
+        sb, se = resofs[i-1], resofs[i]
+        swrs_i += sb
+        assert swrs_i[0, 0] >= sb and swrs_i[-1, -1] <= se
+        swrs_list.append(swrs_i)
+    return np.concatenate(swrs_list, axis=0)
+
+
+def find_and_merge_SWRs(basedir, resofs):
+    """ Finds SWRs in 'basedir' and merges them into one list."""
+    return merge_SWRs(find_SWRs(basedir), resofs)
