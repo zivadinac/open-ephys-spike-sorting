652f3b5529b45ac6c5e8867e53e24b04d6d264b7
Intan Open Ephys <intan@ephys1.ista.local>
2023-04-07 17:16:47 +0200

    Add Vlad's tetrode-only drive.

--- src/implants.py
+++ src/implants.py
@@ -93,10 +93,156 @@ def small_drive_og_rhd_64():
     return layout
 
 
+def big_drive_og_rhd_64():
+    """ Generate layout for JC group's big drive for optogenetics (14x2 tetrodes and 2x2 optic fibers, bilaterally).
+        Channel layout is for Intan RHD 64-channel headstage.
+        Optic fibers are in place of top right and bottom left long shuttles, e.g.
+        in place of the third tetrode in the first row and the second tetrode in the last row."""
+    tetrodes = []
+    tetrodes.append(make_tetrode(0, [12,13,14,15], position=[-3600, 1900]))
+    tetrodes.append(make_tetrode(1, [8,9,10,11], position=[-3600, 2300]))
+    tetrodes.append(make_tetrode(2, [20,21,22,23], position=[-3600, 3100]))
+
+    tetrodes.append(make_tetrode(3, [4,5,6,7], position=[-3950, 2100]))
+    tetrodes.append(make_tetrode(4, [0,1,2,3], position=[-3950, 2500]))
+    tetrodes.append(make_tetrode(5, [24,25,26,27], position=[-3950, 2900]))
+    tetrodes.append(make_tetrode(6, [28,29,30,31], position=[-3950, 3300]))
+
+    tetrodes.append(make_tetrode(7, [60,61,62,63], position=[-4300, 2300]))
+    tetrodes.append(make_tetrode(8, [56,57,58,59], position=[-4300, 2700]))
+    tetrodes.append(make_tetrode(9, [32,33,34,35], position=[-4300, 3100]))
+    tetrodes.append(make_tetrode(10, [36,37,38,39], position=[-4300, 3500]))
+
+    tetrodes.append(make_tetrode(11, [52,53,54,55], position=[-4650, 2500]))
+    tetrodes.append(make_tetrode(12, [40,41,42,43], position=[-4650, 3300]))
+    tetrodes.append(make_tetrode(13, [44,45,46,47], position=[-4650, 3700]))
+
+    tetrodes.append(make_tetrode(14, [80,81,82,83], position=[-3600, -1900]))
+    tetrodes.append(make_tetrode(15, [84,85,86,87], position=[-3600, -2300]))
+    tetrodes.append(make_tetrode(16, [72,73,74,75], position=[-3600, -3100]))
+
+    tetrodes.append(make_tetrode(17, [88,89,90,91], position=[-3950, -2100]))
+    tetrodes.append(make_tetrode(18, [92,93,94,95], position=[-3950, -2500]))
+    tetrodes.append(make_tetrode(19, [68,69,70,71], position=[-3950, -2900]))
+    tetrodes.append(make_tetrode(20, [64,65,66,67], position=[-3950, -3300]))
+
+    tetrodes.append(make_tetrode(21, [96,97,98,99], position=[-4300, -2300]))
+    tetrodes.append(make_tetrode(22, [100,101,102,103], position=[-4300, -2700]))
+    tetrodes.append(make_tetrode(23, [124,125,126,127], position=[-4300, -3100]))
+    tetrodes.append(make_tetrode(24, [120,121,122,123], position=[-4300, -3500]))
+
+    tetrodes.append(make_tetrode(25, [104,105,106,107], position=[-4650, -2500]))
+    tetrodes.append(make_tetrode(26, [116,117,118,119], position=[-4650, -3300]))
+    tetrodes.append(make_tetrode(27, [112,113,114,115], position=[-4650, -3700]))
+
+    layout = ProbeGroup()
+    for tet in tetrodes:
+        layout.add_probe(tet)
+    return layout
+
+
+def igor_drive_og_rhd_64():
+    """ Generate layout for JC group's big drive for optogenetics (14x2 tetrodes and 2x2 optic fibers, bilaterally).
+        Channel layout is for Intan RHD 64-channel headstage.
+        Optic fibers are in place of top right and bottom left long shuttles, e.g.
+        in place of the third tetrode in the first row and the second tetrode in the last row."""
+    tetrodes = []
+    tetrodes.append(make_tetrode(0, [12,13,14,15], position=[-3600, 1900]))
+    tetrodes.append(make_tetrode(1, [16,17,18,19], position=[-3950, 2100]))
+    tetrodes.append(make_tetrode(2, [8,9,10,11], position=[-3600, 2300]))
+    tetrodes.append(make_tetrode(3, [20,21,22,23], position=[-3950, 2500]))
+
+    tetrodes.append(make_tetrode(4, [4,5,6,7], position=[-3600, 3100]))
+    tetrodes.append(make_tetrode(5, [24,25,26,27], position=[-3950, 2900]))
+    tetrodes.append(make_tetrode(6, [0,1,2,3], position=[-3600, 2700]))
+    tetrodes.append(make_tetrode(7, [28,29,30,31], position=[-3950, 3300]))
+
+    tetrodes.append(make_tetrode(8, [60,61,62,63], position=[-4300, 3500]))
+    tetrodes.append(make_tetrode(9, [32,33,34,35], position=[-4650, 3100]))
+    tetrodes.append(make_tetrode(10, [56,57,58,59], position=[-4650, 3500]))
+    tetrodes.append(make_tetrode(11, [36,37,38,39], position=[-4650, 3900]))
+
+    tetrodes.append(make_tetrode(12, [52,53,54,55], position=[-4300, 3100]))
+    tetrodes.append(make_tetrode(13, [40,41,42,43], position=[-4650, 3500]))
+    tetrodes.append(make_tetrode(14, [48,49,50,51], position=[-4300, 2700]))
+    tetrodes.append(make_tetrode(15, [44,45,46,47], position=[-4300, 2300]))
+
+    tetrodes.append(make_tetrode(16, [112,113,114,115], position=[-3600, -1900]))
+    tetrodes.append(make_tetrode(17, [108,109,110,111], position=[-3600, -2300]))
+    tetrodes.append(make_tetrode(18, [116,117,118,119], position=[-3600, -3100]))
+    tetrodes.append(make_tetrode(19, [104,105,106,107], position=[-3600, -3100]))
+
+    tetrodes.append(make_tetrode(20, [120,121,122,123], position=[-3950, -2100]))
+    tetrodes.append(make_tetrode(21, [100,101,102,103], position=[-3950, -2500]))
+    tetrodes.append(make_tetrode(22, [124,125,126,127], position=[-3950, -2900]))
+    tetrodes.append(make_tetrode(23, [96,97,98,99], position=[-3950, -3300]))
+
+    tetrodes.append(make_tetrode(24, [64,65,66,67], position=[-4300, -2300]))
+    tetrodes.append(make_tetrode(25, [92,93,94,95], position=[-4300, -2700]))
+    tetrodes.append(make_tetrode(26, [68,69,70,71], position=[-4300, -3100]))
+    tetrodes.append(make_tetrode(27, [88,89,90,91], position=[-4300, -3500]))
+
+    tetrodes.append(make_tetrode(28, [72,73,74,75], position=[-4650, -2500]))
+    tetrodes.append(make_tetrode(29, [84,85,86,87], position=[-4650, -3300]))
+    tetrodes.append(make_tetrode(30, [76,77,78,79], position=[-4650, -3700]))
+    tetrodes.append(make_tetrode(31, [80,81,82,83], position=[-4650, -3700]))
+
+    layout = ProbeGroup()
+    for tet in tetrodes:
+        layout.add_probe(tet)
+    return layout
+
+
+def vlad_tetrodes():
+    """ Generate layout for Vlad's drive with only tetrodes (12x2 bilaterally).
+        Channel layout is for Intan RHD 64-channel headstage.
+    """
+    tetrodes = []
+    tetrodes.append(make_tetrode(1, [48,49,50,51], position=[-3600, 1900]))
+    tetrodes.append(make_tetrode(2, [56,57,58,59], position=[-3950, 2100]))
+    tetrodes.append(make_tetrode(3, [44,45,46,47], position=[-3600, 2300]))
+
+    tetrodes.append(make_tetrode(4, [36,37,38,39], position=[-3600, 3100]))
+    tetrodes.append(make_tetrode(5, [70,71,72,73], position=[-3950, 2900]))
+    tetrodes.append(make_tetrode(6, [66,67,76,77], position=[-3600, 2700]))
+    
+    tetrodes.append(make_tetrode(7, [52,53,54,55], position=[-3950, 3300]))
+    tetrodes.append(make_tetrode(8, [60,61,62,63], position=[-4300, 3500]))
+    tetrodes.append(make_tetrode(9, [40,41,42,43], position=[-4650, 3100]))
+
+    tetrodes.append(make_tetrode(10, [32,33,34,35], position=[-4650, 3500]))
+    tetrodes.append(make_tetrode(11, [68,69,74,75], position=[-4650, 3900]))
+    tetrodes.append(make_tetrode(12, [64,65,78,79], position=[-4300, 3100]))
+
+    tetrodes.append(make_tetrode(13, [12,13,14,15], position=[-4650, 3500]))
+    tetrodes.append(make_tetrode(14, [4,5,6,7], position=[-4300, 2700]))
+    tetrodes.append(make_tetrode(15, [16,17,18,19], position=[-4300, 2300]))
+
+    tetrodes.append(make_tetrode(16, [24,25,26,27], position=[-3600, -1900]))
+    tetrodes.append(make_tetrode(17, [86,87,88,89], position=[-3600, -2300]))
+    tetrodes.append(make_tetrode(18, [82,83,92,93], position=[-3600, -3100]))
+
+    tetrodes.append(make_tetrode(19, [8,9,10,11], position=[-3600, -3100]))
+    tetrodes.append(make_tetrode(20, [0,1,2,3], position=[-3950, -2100]))
+    tetrodes.append(make_tetrode(21, [20,21,22,23], position=[-3950, -2500]))
+
+    tetrodes.append(make_tetrode(22, [28,29,30,31], position=[-3950, -2900]))
+    tetrodes.append(make_tetrode(23, [90,91,84,85], position=[-3950, -3300]))
+    tetrodes.append(make_tetrode(24, [80,81,94,95], position=[-4300, -2300]))
+
+    layout = ProbeGroup()
+    for tet in tetrodes:
+        layout.add_probe(tet)
+    return layout
+
+
 ## use this two vars to predefine implants
-DEFINED_IMPLANTS = ["small_drive_rhd_64", "small_drive_og_rhd_64"]
-__def_implants_funcs = {"small_drive_rhd_64": small_drive_rhd_64,\
-                        "small_drive_og_rhd_64": small_drive_og_rhd_64}
+__def_implants_funcs = {"small_drive_rhd_64": small_drive_rhd_64,
+                        "small_drive_og_rhd_64": small_drive_og_rhd_64,
+                        "big_drive_og_rhd_64": big_drive_og_rhd_64,
+                        "igor_drive_og_rhd_64": igor_drive_og_rhd_64,
+                        "vlad_tetrodes": vlad_tetrodes}
+DEFINED_IMPLANTS = list(__def_implants_funcs.keys())
 
 def get_implant(name):
     if name not in DEFINED_IMPLANTS:
@@ -117,6 +263,8 @@ def plot_implant_layout(layout, path=None):
     for tet in layout.probes:
         pos = np.mean(tet.contact_positions, axis=0)
         plt.text(*pos, tet.annotations["name"], ha='center', va='center', fontweight="demibold", fontsize=9)
+        for cp, ci in zip(tet.contact_positions, tet.device_channel_indices):
+            plt.text(*cp, str(ci), ha='center', va='center', fontsize=7, color="white")
     if path is not None:
         plt.savefig(path)
     else:
